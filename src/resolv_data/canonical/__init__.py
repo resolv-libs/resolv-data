896c9ee71dbf368a499ef7ebf2052eef7ec0d4a3
Matteo Pettenò <matteo.petteno1@gmail.com>
2024-03-07 18:47:56 +0100

    + Get canonical format from source type

--- src/resolv_data/canonical/__init__.py
+++ src/resolv_data/canonical/__init__.py
@@ -6,4 +6,5 @@ License: Apache License 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
 from . import adapters
 from . import exceptions
 from .base import CanonicalFormat, DataAdapter
-from .wrapper import to_canonical_format, to_source_format
+from .wrapper import get_canonical_format_by_source_type, get_data_adapter_by_source_type, to_canonical_format, \
+    to_source_format
--- src/resolv_data/canonical/wrapper.py
+++ src/resolv_data/canonical/wrapper.py
@@ -1,5 +1,6 @@
 """ This module provides functions to use for converting source data to a canonical format and vice
 versa. It is meant to be used as the only enry point in the canonicalization process. """
+import typing
 from typing import Any, Type, List, Dict
 
 from .adapters.mir.symbolic_music.midi import MIDIFileAdapter
@@ -14,22 +15,38 @@ SOURCE_TYPE_DATA_ADAPTER_MAP: Dict[Type[DataAdapter], List[str]] = {
 }
 
 
-def resolve_data_adapter_by_source_type(source_type: str) -> Type[DataAdapter]:
-    """ Resolve the appropriate data adapter based on the source type.
+def get_canonical_format_by_source_type(source_type: str) -> CanonicalFormat:
+    """ Get the canonical format associated to the source type.
 
     Args:
         source_type (str): The type of the source file.
 
     Returns:
-        (Type[DataAdapter]): The data adapter class corresponding to the source type.
+        (CanonicalFormat): The canonical format to the source type.
 
     Raises:
-        ValueError: If the source type is unrecognized.
+        ValueError: If the source type is unsupported.
     """
-    for entry_type, extensions_list in SOURCE_TYPE_DATA_ADAPTER_MAP.items():
+    data_adapter = get_data_adapter_by_source_type(source_type)
+    return typing.get_args(data_adapter.__orig_bases__[0])
+
+
+def get_data_adapter_by_source_type(source_type: str) -> DataAdapter:
+    """ Get the appropriate data adapter based on the source type.
+
+    Args:
+        source_type (str): The type of the source file.
+
+    Returns:
+        (DataAdapter): The data adapter corresponding to the source type.
+
+    Raises:
+        ValueError: If the source type is unsupported.
+    """
+    for data_adapter, extensions_list in SOURCE_TYPE_DATA_ADAPTER_MAP.items():
         if source_type in extensions_list:
-            return entry_type
-    raise ValueError(f'Unrecognized file type: {source_type}')
+            return data_adapter()
+    raise ValueError(f'Unsupported file type: {source_type}')
 
 
 def to_canonical_format(source_type: str, content: bytes, metadata: Any) -> CanonicalFormat:
@@ -47,8 +64,8 @@ def to_canonical_format(source_type: str, content: bytes, metadata: Any) -> Cano
         CanonicalizationError: If an error occurs during canonicalization.
     """
     try:
-        data_adapter = resolve_data_adapter_by_source_type(source_type)
-        return data_adapter().to_canonical_message(source_type, content, metadata)
+        data_adapter = get_data_adapter_by_source_type(source_type)
+        return data_adapter.to_canonical_message(source_type, content, metadata)
     except Exception as e:
         raise CanonicalizationError(e)
 
@@ -63,5 +80,5 @@ def to_source_format(source_type: str, canonical_message: CanonicalFormat, **kwa
     Returns:
         (bytes): The content of the source file in the specified format.
     """
-    data_adapter = resolve_data_adapter_by_source_type(source_type)
-    return data_adapter().to_source_format(canonical_message, **kwargs)
+    data_adapter = get_data_adapter_by_source_type(source_type)
+    return data_adapter.to_source_format(canonical_message, **kwargs)
