c0c290ddce4e85ce05f39b07ef588ba7364705db
Matteo Pettenò <matteo.petteno1@gmail.com>
2024-03-29 15:41:17 +0100

    + Test improvement

--- tests/datasets/test_jsb_chorales.py
+++ tests/datasets/test_jsb_chorales.py
@@ -1,5 +1,6 @@
 import json
 import unittest
+from pathlib import Path
 
 from google.protobuf.json_format import Parse
 
@@ -7,11 +8,16 @@ from resolv_data import DatasetIndex
 from resolv_data.datasets.mir import JSBChoralesDataset
 from resolv_data.scripts.import_dataset import import_dataset
 
-INDEXES_DIR = "./indexes"
-OUTPUT_DIR = "./output"
-
 
 class JSBChoralesDatasetTest(unittest.TestCase):
+
+    @property
+    def indexes_dir(self) -> Path:
+        return Path("./datasets/indexes")
+
+    @property
+    def output_dir(self) -> Path:
+        return Path("./datasets/output")
 
     def test_download_and_index(self):
         dataset_modes = JSBChoralesDataset.remote_sources().keys()
@@ -19,14 +25,14 @@ class JSBChoralesDatasetTest(unittest.TestCase):
             dataset_root_dir, dataset_index = import_dataset(
                 dataset_name='jsb-chorales-v1',
                 dataset_mode=mode,
-                output_path=OUTPUT_DIR,
+                output_path=self.output_dir,
                 temp=False,
                 overwrite=True,
                 cleanup=True,
                 allow_invalid_checksum=False
             )
             self.assertTrue(dataset_root_dir.exists())
-            with open(f'{INDEXES_DIR}/jsb_chorales_v1_index.json', 'r') as file:
+            with open(f'{self.indexes_dir}/jsb_chorales_v1_index.json', 'r') as file:
                 json_index = json.load(file)
                 jsb_chorales_index = Parse(json.dumps(json_index), DatasetIndex())
                 self.assertEqual(dataset_index, jsb_chorales_index)
